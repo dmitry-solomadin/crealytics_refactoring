039e9b8da7de3bbb1fbf814a6351ef53f02793d8
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2016-02-04 14:25:17 +0200

    Added README + some refactorings to file writing mechanism

---
+++ README.md
@@ -0,0 +1,29 @@
+There are couple of things I would like to say about this task:
+1. Refactoring should not be done if you have no tests or no way of understanding what code does. If you'd
+supplied test file with the task then it would be ok, instead I had to guess what this code actually does and guess
+the format of the file. This task was very far off real world task and if I'd encounter a piece of code that
+works but nobody knows how it works, there are no tests and nobody could even run that code on real data —
+the only correct decision here is to leave it be. Because the probability of missing some edge case scenarios when
+reading the code w/o ability to test on real data is too high.
+2. It seems like Combiner class was not used at all. It was combining two iterators by certain key if the values were
+matching, but code always supplied only 1 iterator so I've decided to remove it entirely. Everything else - methods like:
+combine_hashes and combine_values - were removed because of this change. I'm not sure that was correct change since
+I had no real data to test on.
+3. I think the purpose of this code is next:
+* It gets latest ad campaigns performance data on the project (there was a bug there actually)
+* Then it sorts that data by 'Clicks' column
+* If we had multiple data files then it would combine data from multiple files by 'Keyword Unique ID' and made some
+ manipulations based column, for certain columns it would've choosen just the last value for others - first
+ but since code works only with single data file there is no data combining.
+* Then it will make modifications to certain columns multiplying them by different factors.
+* Finally it saves data into output file.
+So generally, what it does right now is pretty simple: sort, some columns * factors, save
+4. I might be mistaken in my initial assumptions, like I've said there is a risk a missed something, if that's the case
+then my refactoring is completely wrong, I justify that by having no means to test it on real data :)
+5. If I had more time I would've tried to remove multiple enumerators, while it makes code look neat, but it renders
+it less readable, I believe it could greatly simplified.
+6. If I had data files to work with initially I also would've used different approach to the refactoring process.
+I would've written tests on existing code first and then refactor it. But because I didn't know the input/output of the code.
+I had to debug it and it was much easier to do refactor along the way. Also writing tests prior to the refactoring w/o
+knowing input/output has no benefit since those tests would've been just my assumptions.
+
--- modifier.rb
+++ modifier.rb
@@ -21,8 +21,8 @@ class Modifier
 
   LINES_PER_FILE = 120000
 
-	def initialize(saleamount_factor, cancellation_factor)
-		@saleamount_factor = saleamount_factor
+	def initialize(modification_factor:, cancellation_factor:)
+		@modification_factor = modification_factor
 		@cancellation_factor = cancellation_factor
 	end
 
@@ -31,94 +31,40 @@ class Modifier
 
 		input_enumerator = CSVHelper.lazy_read(input)
 
-    # TODO: Not sure we need that at all
-		combiner = Combiner.new do |value|
-			value[KEYWORD_UNIQUE_ID]
-    end.combine(input_enumerator)
+    elements = input_enumerator.map { |el| combine_values(el.to_hash) }
 
-		merger = Enumerator.new do |yielder|
-			while true
-				begin
-					list_of_rows = combiner.next
-					merged = combine_hashes(list_of_rows)
-					yielder.yield(combine_values(merged))
-				rescue StopIteration
-					break
-				end
-			end
-    end
-
-    write_file(merger, output)
+    write_file(elements, output)
 	end
 
 	private
 
-  def write_file(merger, output)
-    done = false
-    file_index = 0
+  def write_file(elements, output)
     file_name = output.gsub('.txt', '')
-    while !done do
-      CSV.open(file_name + "_#{file_index}.txt", "wb", { :col_sep => "\t", :headers => :first_row, :row_sep => "\r\n" }) do |csv|
-        headers_written = false
-        line_count = 0
-        while line_count < LINES_PER_FILE
-          begin
-            merged = merger.next
-            unless headers_written
-              csv << merged.keys
-              headers_written = true
-              line_count += 1
-            end
-            csv << merged
-            line_count += 1
-          rescue StopIteration
-            done = true
-            break
-          end
-        end
-        file_index += 1
-      end
+    elements.each_slice(LINES_PER_FILE).with_index do |values, index|
+      CSVHelper.write(values, elements.first.keys, file_name + "_#{index}.txt")
     end
   end
 
 	def combine_values(hash)
 		LAST_VALUE_WINS.each do |key|
-			hash[key] = hash[key].last
+			hash[key] = hash[key]
 		end
 		LAST_REAL_VALUE_WINS.each do |key|
-			hash[key] = hash[key].select {|v| not (v.nil? or v == 0 or v == '0' or v == '')}.last
+			hash[key] = hash[key]
 		end
 		INT_VALUES.each do |key|
-			hash[key] = hash[key][0].to_s
+			hash[key] = hash[key].to_s
 		end
 		FLOAT_VALUES.each do |key|
-			hash[key] = hash[key][0].from_german_to_f.to_german_s
+			hash[key] = hash[key].from_german_to_f.to_german_s
 		end
     COMMISSION_VALUES.each do |key|
-			hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
+			hash[key] = (@cancellation_factor * hash[key].from_german_to_f).to_german_s
 		end
     COMMISSION_VALUES_OTHER.each do |key|
-			hash[key] = (@cancellation_factor * @saleamount_factor * hash[key][0].from_german_to_f).to_german_s
+			hash[key] = (@cancellation_factor * @modification_factor * hash[key].from_german_to_f).to_german_s
 		end
 		hash
-	end
-
-	def combine_hashes(list_of_rows)
-		keys = []
-		list_of_rows.each do |row|
-			next if row.nil?
-			row.headers.each do |key|
-				keys << key
-			end
-		end
-		result = {}
-		keys.each do |key|
-			result[key] = []
-			list_of_rows.each do |row|
-				result[key] << (row.nil? ? nil : row[key])
-			end
-		end
-		result
 	end
 
 	def sort_by_clicks(file)
@@ -135,9 +81,7 @@ end
 # Note: un-refactored code had a bug when the file with strict data was rendered
 # I've fixed it by reading actually latest data file for project.
 modified = input = PerformanceDataGetter.latest_for_project('project_2012-07-27_*')
-modification_factor = 1
-cancellation_factor = 0.4
-modifier = Modifier.new(modification_factor, cancellation_factor)
+modifier = Modifier.new(modification_factor: 1, cancellation_factor: 0.4)
 modifier.modify(modified, input)
 
 puts "DONE modifying"
